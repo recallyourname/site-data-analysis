f97951e317d46ab83e484af61c5d417c6ae91645
recallyourname <vedkalnikita@gmail.com>
2021-04-16 14:22:54 +0600

    'deep' search implementation

--- project/main.py
+++ project/main.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 from urllib import request, error
 from urllib.request import Request, urlopen
 from nltk.tokenize import RegexpTokenizer
+from urllib.error import HTTPError
 
 tokenizer = RegexpTokenizer(r'\w+')
 
@@ -15,6 +16,27 @@ class SiteAnalyzer:
 
     def __init__(self):
         pass
+    
+    def url_validator(self, url):
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc, result.path])
+        except:
+            return False
+
+    def parse_page_for_subpages(self, url, search_list):
+
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        raw = BeautifulSoup(webpage, 'html.parser')
+
+        for link in raw.find_all('a'):
+            temp = link.get('href')
+            if self.url_validator(temp) is False and temp is not None :
+                temp = url+str(temp).removeprefix("/")
+
+            if self.url_validator(temp) and url in temp and temp not in search_list:
+                search_list.append(temp)
 
     def get_soup(self, url):
         try:
@@ -41,13 +63,6 @@ class SiteAnalyzer:
 
     def import_data_to_excel():
         pass
-    
-    def url_validator(self, url):
-        try:
-            result = urlparse(url)
-            return all([result.scheme, result.netloc, result.path])
-        except:
-            return False
 
 def main(argv=None):
     analyzer = SiteAnalyzer()
@@ -55,42 +70,58 @@ def main(argv=None):
     #     argv = sys.argv
     # urls = []
     argv = [
+        'deep',
         'https://www.apptunix.com/',
         'https://trio.dev/',
         'https://youteam.io/',
         'https://www.daxx.com/',
         'https://distantjob.com/',
         'https://relevant.software/',
-        'https://remotemore.com/',
-        'https://www.peerbits.com/',
-        'https://codersera.com/',
-        'https://www.makeitinua.com',
-        'https://soshace.com/',
-        'https://intersog.com/',
-        'https://x-team.com/',
-        'https://remotedevjobs.xyz/',
-        'https://www.quickmonday.com/',
-        'https://remoteok.io/',
-        'https://hackernoon.com/',
-        'https://www.classicinformatics.com',
-        'https://www.remoteco.com/',
-        'https://remoteplatz.com/',
-        'https://stormotion.io/',
-        'https://www.trustshoring.com/',
-        'https://geektastic.com/',
-        'https://geektastic.com/about',
+        # 'https://remotemore.com/',
+        # 'https://www.peerbits.com/',
+        # 'https://codersera.com/',
+        # 'https://www.makeitinua.com',
+        # 'https://soshace.com/',
+        # 'https://intersog.com/',
+        # 'https://x-team.com/',
+        # 'https://remotedevjobs.xyz/',
+        # 'https://www.quickmonday.com/',
+        # 'https://remoteok.io/',
+        # 'https://hackernoon.com/',
+        # 'https://www.classicinformatics.com',
+        # 'https://www.remoteco.com/',
+        # 'https://remoteplatz.com/',
+        # 'https://stormotion.io/',
+        # 'https://www.trustshoring.com/',
+        # 'https://geektastic.com/',
+        # 'https://geektastic.com/about',
         'https://geektastic.com/blog'
     ]
-    
-    for arg in argv:
-        if analyzer.url_validator(arg):
-            _ = analyzer.get_soup(arg)
+    temp_list = []
+    for link in argv:
+        if analyzer.url_validator(link):
+            temp_list.append(link)
+    search_list = []
+    if 'deep' in argv:
+        for link in temp_list:
+            try:
+                analyzer.parse_page_for_subpages(link, search_list)
+            except HTTPError:
+                pass
+    else: 
+        search_list = temp_list
+
+    for link in search_list:
+        if analyzer.url_validator(link):
+            
+            _ = analyzer.get_soup(link)
             if _ is None:
                 continue
             _ = analyzer.tokenize_soup(_)
             _ = analyzer.textify_data(_)
             fd = nltk.FreqDist(_)
-            print(fd.tabulate(10))
+            print(link)
+            fd.tabulate(5)
 
 if __name__ == "__main__":
     main()
